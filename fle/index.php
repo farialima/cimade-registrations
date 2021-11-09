2b884c63b07973c21a4864af0c5393ee29d3abb0
François Granade <github.com@farialima.net>
2021-11-09 06:15:11 -0500

    recent info for FLE

--- fle/index.php
+++ fle/index.php
@@ -12,6 +12,26 @@
   <body>
     <h3 class="text">Cimade Lyon</h3>
     <h4>Rendez-vous d'inscription pour les cours de français 2021-2022</h4>
+<p></p>
+<p>Nous organisons de nouvelles sessions d'inscription aux cours de français. Ce sera:
+  <ul>
+    <li>Mercredi 10 novembre 2021, de 14 à 17h.</li>
+    <li>Jeudi 11 novembre 2021, de 14 à 17h.</li>
+<!--
+    <li>Lundi 3 janvier 2022, de 13h à 16h.</li>
+    <li>Vendredi 7 janvier, de 13h à 16h.</li>
+    <li>Samedi 8 janvie, de 13h à 16h.</li>
+    <li>Lundi 10 janvier, de 13h à 16h.</li>
+    <li>Vendredi 14 janvie, de 13h à 16h.</li>
+    <li>Samedi 15 janvier, de 13h à 16h.</li>
+    <>Lundi 28 février, de 13h à 16h.</li>
+    <li>Vendredi 4 mars, de 13h à 16h.</li>
+    <li>Samedi 5 mar, de 13h à 16h.</li>
+    <li>Lundi 11 avril, de 13h à 16h.</li>
+    <li>Vendredi 15 avril, de 13h à 16h.</li> -->
+</ul>
+</p>
+<p>Vous pouvez venir à ces moments, au 33 Rue Imbert-Colomès (Lyon 1er), sans rendez-vous.</p>
     <?php
    include '../functions.php';
    date_default_timezone_set('Europe/Paris');
@@ -39,8 +59,14 @@
    do_page($csv_file, $days, 
            "<p>Pour venir vous informer et vous inscrire, il faut prendre rendez-vous. Merci d’écrire votre nom, prénom, et votre téléphone ou email&nbsp;; et de choisir un jour et une heure de rendez-vous.</p>\n      <p>Venez au 33 Rue Imbert-Colomès (Lyon 1er) le jour et l’heure de votre rendez-vous. Merci !</p>\n<p>Plus d'information sur les cours de français <a href=\"https://www.lacimade.org/activite/les-ateliers-socio-linguistiques-a-s-l/\">ici</a>.</p>",
            '<p>Venez à l’heure au 33 Rue Imbert-Colomès (Lyon 1er). <b>Masque obligatoire</b> mais nous pourrons vous en fournir si vous n’en avez pas :).</p><p>Si vous ne pouvez pas venir, merci de nous envoyer un mail à <a href="mailto:fle.lyon@lacimade.org">fle.lyon@lacimade.org</a> pour annuler.</p>',
-           6);
+6,
+"<p>Si vous ne pouvez pas venir à ces dates: nous organiserons certainements de nouvelles sessions d’information et d’inscription, en debut 2022. ".
+          "Vous pouvez laisser vos nom, prénom, et téléphone ou email, ".
+          "et nous vous contacterons si et quand nous en organiserons.</p>"
+);
    ?>
+
+
 <!--  <p>Les cours de français Cimade dans nos locaux sont suspendus depuis le 30 octobre 2020 en raison de la situation sanitaire, qui  impacte largement notre capacité à déployer nos activités habituelles. De nouvelles informations seront données sur cette page lorsqu’il nous sera possible de reprendre une activité normale. Nous nous excusons de ne pouvoir vous ouvrir nos portes pour l’instant, mais nous espérons pouvoir malgré tout vous recevoir à nouveau prochainement, pour de nouvelles inscriptions et une reprise des cours.
           </p>  -->
   </body>
